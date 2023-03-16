41676866238ede26efce2cf696df811d9d8a57a6
Dharmender Srivastav <d.srivastav@sportradar.com>
2023-03-16 16:39:56 +0100

    Additional wait

--- cypress/fixtures/AppActions/Comparison.js
+++ cypress/fixtures/AppActions/Comparison.js
@@ -35,8 +35,8 @@ export const validateDataWithHeaderEvents = (event) => {
 }
 
 export const selectTeamFilter = (filter, option) => {
-    cy.get('.mr-4').contains(filter).then(($elem) => {
+    cy.wait(3000).get('.mr-4').contains(filter).then(($elem) => {
         cy.wrap($elem).parent().find(Comparison.COM_SELECT_FILTER).click()
-        cy.get(Comparison.COM_SELECT_FILTER_OPT).contains(option).click({multiple:true})
+        cy.get(Comparison.COM_SELECT_FILTER_OPT).contains(option).wait(3000).click({multiple:true})
     })
 }
