6c7f3372ba88e7665927b3cd2b1d772e5846e0b8
Dharmender Srivastav <d.srivastav@sportradar.com>
2023-05-18 10:35:44 +0200

    Added additional test for downloading export files

--- cypress/e2e/test/functional/teams.leaderboards.spec.js
+++ cypress/e2e/test/functional/teams.leaderboards.spec.js
@@ -1,19 +1,25 @@
 import {teamsiteLogin} from "../../../fixtures/AppActions/login.js"
-import {validateTeamsLeaderboards, searchAndSelectTeam, selectReportType} from "../../../fixtures/AppActions/leaderboards.js"
+import {validateTeamsLeaderboards, searchAndSelectTeam, selectReportType, verifyDownloadReports} from "../../../fixtures/AppActions/leaderboards.js"
 
 describe('Game Tab overview check', () => {
 
       const team = 'Golden State Warriors'
-      const tabs = ['Extended Box Score','Play Types','Play by Play','Game Breakdown','Shot Chart','Lineups']
-      const player = 'Jordan Poole '
-      const clips = ['All Clips','All Possessions','Full Game Video']
-      const league = 'NBA'
-      const season = '2022-2023'
+      const filename = 'Leaderboards'
+      const format = ["XLSX","CSV"]
 
 
       beforeEach(() => {
         cy.visit(Cypress.env('URL'))
         teamsiteLogin(Cypress.env('USERNAME_TEAM'),Cypress.env('PASSWORD_TEAM'))
+      })
+
+      it("Leaderboard stats can be downloaded from Teams leaderboard page", () => {
+        cy.openSubHeader('Leaderboards','Team Leaderboards')
+        validateTeamsLeaderboards('Team Offensive')
+        selectReportType('Team Defensive')
+        format.forEach((item) =>{
+          verifyDownloadReports(item,filename)
+        })
       })
 
       it("Stats can be viewed under Team leaderboards", () => {
