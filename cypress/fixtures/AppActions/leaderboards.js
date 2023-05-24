cdbb2181962261f3acda48f00231799e9fec4f81
Dharmender Srivastav <d.srivastav@sportradar.com>
2023-05-24 10:32:26 +0200

    Added flows for Player leaderboards

--- cypress/fixtures/AppActions/leaderboards.js
+++ cypress/fixtures/AppActions/leaderboards.js
@@ -1,19 +1,21 @@
 import Leaderboards from "../Locators/leaderboards.js"
 
-export const validateTeamsLeaderboards = (reportType) => {
+export const validateLeaderboardsPage = (reportType, ) => {
         cy.get(Leaderboards.LEADER_HEADER).find('.ng-value-container .ng-value-label').should('have.text',reportType)
-        cy.get('.ts-header-row').should('include.text', 'Team')
 }
 
-export const searchAndSelectTeam = (team) => {
-    cy.get(Leaderboards.LEADER_SEARCH_BOX).clear().type(team)
-    cy.get(Leaderboards.LEADER_RESULT_ROWS).should('include.text',team)
-    cy.get(Leaderboards.LEADER_RESULT_ROWS).contains(team).click()
-    cy.wait(4000).url().should('contain','teams')
+export const searchAndSelect = (type, option) => {
+    cy.get(Leaderboards.LEADER_SEARCH_BOX).clear({force:true}).type(option)
+    cy.get(Leaderboards.LEADER_RESULT_ROWS).should('include.text',option)
+    cy.get(Leaderboards.LEADER_RESULT_ROWS).contains(option).click()
+    if(type === 'Team')
+        cy.wait(4000).url().should('contain','teams')
+    else
+        cy.wait(4000).url().should('contain','players')
 }
 
 export const selectReportType = (reportType) => {
-    cy.get(Leaderboards.LEADER_HEADER).find('.ng-value-container').contains('Team').click({force: true})
+    cy.get(Leaderboards.LEADER_HEADER).find('.ng-value-container').contains('Offensive').click({force: true})
     cy.wait(4000).get('.ng-dropdown-panel-items .ng-option-label').contains(reportType).click()
 }
 
@@ -24,7 +26,7 @@ export const verifyDownloadReports = (format, filename) => {
 }
 
 export const validatePossessionStats = (team, type, stat, value) => {
-    cy.get(Leaderboards.LEADER_SEARCH_BOX).clear().type(team)
+    cy.get(Leaderboards.LEADER_SEARCH_BOX).clear({force:true,waitForAnimations: true}).type(team)
     if(type === 'Total'){
         cy.get('.toggle-root').contains('Possessions:').then(($elem) =>{
            cy.wrap($elem).parent().find('.ts-toggle-slider').click()
