c5b1701b8a4790d51b7986c8473305ab07057988
Dharmender Srivastav <d.srivastav@sportradar.com>
2023-07-07 14:54:48 +0200

    Added tests for Grassroot

--- cypress/e2e/test/functional/grassroots.spec.js
+++ cypress/e2e/test/functional/grassroots.spec.js
@@ -1,9 +1,10 @@
 import {teamsiteLogin} from "../../../fixtures/AppActions/login.js"
 import {clickTab} from "../../../support/commands.js"
-import {selectEvent, validatePlayer} from "../../../fixtures/AppActions/grassroots.js"
+import {selectEvent, selectDivision, selectTeam, validatePlayer, selectPlayer, validatePlayerPage} from "../../../fixtures/AppActions/grassroots.js"
 
 describe('Leaderboards Tab overview check', () => {
 
+      var playerStat
       var team
       var event
       var player
@@ -14,29 +15,30 @@ describe('Leaderboards Tab overview check', () => {
         teamsiteLogin(Cypress.env('USERNAME_TEAM'),Cypress.env('PASSWORD_TEAM'))
       })
 
-     it("Player positions can be viewed under 2021 Nike EYBL",() => {
+     it("Player positions can be viewed under 2022-23 NBA Academy",() => {
           event = '2022-23 NBA ACADEMY'
-          player = 'Khaman Maluach  South  Sudan  C  2025  Africa'
+          playerStat = 'Augusto Cassia  Brazil  SF  2023  Global'
+          player = 'Augusto Cassia'
 
           cy.clickTab('Grassroots')
           selectEvent(event)
-          validatePlayer(player)
-//          selectClick(player)
-//          validatePlayerPage()
+          validatePlayer(playerStat)
+          selectPlayer(player)
+          validatePlayerPage()
      })
 
-//     it("Player positions can be viewed under 2021 Nike EYBL",() => {
-//        event = '2021 NIKE EYBL'
-//        division = '15U'
-//        team = 'Alabama Fusion 15U'
-//        player = 'Khaman Maluach South Sudan	71 C 2025	Africa'
-//
-//        selectEvent(event)
-//        selectDivision(division)
-//        selectTeam(team)
-//        validatePlayer(player)
-//        selectClick(player)
-//        validatePlayerPage()
-//     })
+     it("Player positions can be viewed under 2021 Nike EYBL",() => {
+        event = '2021 NIKE EYBL'
+        division = '15U'
+        team = 'Alabama Fusion 15U'
+        player = 'Tommy Tisdale'
+
+        cy.clickTab('Grassroots')
+        selectEvent(event)
+        selectDivision(division)
+        selectTeam(team)
+        selectPlayer(player)
+        validatePlayerPage()
+     })
 
 })
--- cypress/fixtures/AppActions/grassroots.js
+++ cypress/fixtures/AppActions/grassroots.js
@@ -1,20 +1,24 @@
 import Grassroots from "../Locators/grassroots.js"
 
-//Reusable user actions to chain them to form e2e flow
-
 export const selectEvent = (event) => {
-        cy.get(Grassroots.GRASS_EVENT).click({multiple:true})
-        cy.get(Grassroots.GRASS_EVENT_OPTIONS).contains(event).click()
+        cy.get(Grassroots.GRASS_EVENT).contains('Event:').then((elem) => {
+            cy.wrap(elem).get('.ng-select-searchable').click({multiple:true})
+        })
+        cy.wait(3000).get(Grassroots.GRASS_EVENT_OPTIONS).contains(event).click({force: true})
 }
 
 export const selectDivision = (division) => {
-        cy.get(Grassroots.GRASS_EVENT).click({multiple:true})
-        cy.get(Grassroots.GRASS_EVENT_OPTIONS).contains(event).click()
+        cy.get(Grassroots.GRASS_EVENT).contains('Division:').then((elem) => {
+            cy.wrap(elem).get('.ng-select-searchable').click({multiple:true})
+        })
+        cy.wait(3000).get(Grassroots.GRASS_EVENT_OPTIONS).contains(division).click({force: true})
 }
 
 export const selectTeam = (team) => {
-        cy.get(Grassroots.GRASS_EVENT).click({multiple:true})
-        cy.get(Grassroots.GRASS_EVENT_OPTIONS).contains(event).click()
+        cy.get(Grassroots.GRASS_EVENT).contains('Team:').then((elem) => {
+              cy.wrap(elem).get('.ng-select-searchable').click({multiple:true})
+        })
+        cy.wait(3000).get(Grassroots.GRASS_EVENT_OPTIONS).contains(team).click({force: true})
 }
 
 export const selectEntry = (option) => {
@@ -24,18 +28,23 @@ export const selectEntry = (option) => {
 
 export const validatePlayer = (player) => {
     const arr = player.split('  ')
-//    const text =
-    console.log(cy.get(Grassroots.GRASS_TABLE_ROWS).contains(arr[0]).invoke('text'))
+    cy.contains("td",arr[0])
+      .invoke("index")
+          .then((index) => {
+            cy.contains("td",arr[1])
+            .should('have.text',' ' + arr[1] + ' ')
+            cy.contains("td",arr[2])
+            .should('have.text',' ' + arr[2] + ' ')
+            cy.contains("td",arr[3])
+            .should('have.text',' ' + arr[3] + ' ')
+    })
 
-//    contains("td",arr[0])
-//      .invoke("index")
-//          .then((index) => {
-//            cy.contains("td",arr[1])
-//            .invoke('text')
-//            .should('eq',arr[1])
-//            .and('eq',arr[2])
-//            .and('eq',arr[3])
-//    })
+}
 
+export const selectPlayer = (player) => {
+    cy.get(Grassroots.GRASS_TABLE_ROW_LINKS).contains(player).click({force: true})
+}
 
+export const validatePlayerPage = () => {
+    cy.wait(4000).url().should('contain','players')
 }
